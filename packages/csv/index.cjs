a52961bdda5707ce61472402fd44fc786969906e
Archie Wood <58074498+archiewood@users.noreply.github.com>
2022-09-19 21:49:21 -0400

    returning to command line but not to runQuery

--- packages/csv/index.cjs
+++ packages/csv/index.cjs
@@ -4,33 +4,22 @@ const { processQueryResults } = require('@evidence-dev/db-commons')
 
 
 const runQuery = async (queryString, database) => {
-    console.log("running csv query")
     
     const filename = database ? path.parse(database.filename).name  : process.env["CSV_FILENAME"] || process.env["csv_filename"] || process.env["CSV_FILENAME"]
-    const filepath = filename !== ":memory:" ? "../../" + filename : filename
+    const filepath = filename !== ":memory:" ? "../../" + filename + ".csv" : filename
     try {
-        console.log("\n trying to create csv db")
-        const db = new duckdb.Database(filename + ".duckdb")
-        var creationString = "CREATE TABLE " + filename + " FROM '" + filepath +".csv'";
-        console.log("creation string is: " + creationString)
-        db.all(creationString)
-
-        console.log(db.all("select * from orders limit 1"))
+        const db = new duckdb.Database(":memory:");
+        db.all(queryString, function(error, result){
+            if (error) {
+                    throw error
+                }
+            console.log(processQueryResults(result))
+            return processQueryResults(result);
+            });  
         
-
-        const result = await db.all(queryString);
-        return processQueryResults(result);
-    } catch(err) {
-        if (err.message) {
-            if(err.errno === 14){
-                throw "Unable to open " + filename + " in " + path.resolve("../../")
-            } else {
-                throw err.message
-            }
-        } else {
-            throw err
+        } catch(err) {
+            throw err.message
         }
-    }
 }
 
 module.exports = runQuery
---
+++ sites/example-project/src/pages/duckdb-test.md
@@ -0,0 +1,5 @@
+# Duckdb testing as a csv connection
+
+```all
+select * from '../../unit_saving_data.csv' limit 3
+```
